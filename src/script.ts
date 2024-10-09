629abbdfa54f7a0aea731e9b2bdb4f84fddd94d6
Jorge Sant Ana <jorge@argus-academy.com>
2024-10-09 09:42:28 -0300

    :tada: feat: Executando os scripts compilados no Browser

--- src/script.ts
+++ src/script.ts
@@ -1,6 +1,3 @@
-export function sum(a: number, b: number) {
-  console.log('Dentro do escopo do módulo');
-  return {
-    result: a + b,
-  };
+export function bootstrap() {
+  console.log('Script carregado com sucesso');
 }
