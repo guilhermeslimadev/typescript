966d3f8aba318640c494e9225caf3a429dc0492d
Jorge Sant Ana <jorge@argus-academy.com>
2024-10-15 10:18:52 -0300

    :tada: feat: Type Guard

---
+++ src/modules/narrowing/type-guard.ts
@@ -0,0 +1,14 @@
+export const bootstrap = (): void => {
+  const zipCodeMask = (value: string | number): string => {
+    value = value.replace(/\D/g, '');
+    value = value.replace(/(\d{5})(\d)/, '$1-$2');
+
+    return value;
+  };
+
+  const zipCode = zipCodeMask('10000000'); //10000-000
+  console.log(zipCode);
+
+  const zipCode2 = zipCodeMask(20000000); //20000-000
+  console.log(zipCode2);
+};
