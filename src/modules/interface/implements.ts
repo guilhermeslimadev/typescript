89b17af74d061e81ebb95e13ba2508971f1a390f
Jorge Sant Ana <jorge@argus-academy.com>
2024-10-30 09:53:43 -0300

    :tada: feat: Implementando Interfaces (Implements)

---
+++ src/modules/interface/implements.ts
@@ -0,0 +1,50 @@
+export const bootstrap = (): void => {
+  interface Resume {
+    fullName: string;
+    email: string;
+    skills: Skill[];
+    addSkill: (skill: Skill) => boolean;
+  }
+
+  interface Skill {
+    name: string;
+    level: 'beginner' | 'intermediate' | 'advanced';
+  }
+
+  class MyResume implements Resume {
+    constructor(
+      public fullName: string,
+      public email: string,
+      public skills: Skill[],
+    ) {}
+
+    addSkill(skill: Skill): boolean {
+      const initialLength = this.skills.length;
+      this.skills.push(skill);
+      return this.skills.length > initialLength;
+    }
+  }
+
+  const myResume = new MyResume(
+    'Jorge Sant Ana',
+    'jorge@argus-academy.com',
+    [],
+  );
+
+  myResume.addSkill({ name: 'JavaScript', level: 'advanced' });
+  myResume.addSkill({ name: 'TypeScript', level: 'advanced' });
+  console.log(myResume);
+
+  /*
+  const MyResume: Resume = {
+    fullName: 'Jorge Sant Ana',
+    email: 'jorge@argus-academy.com',
+    skills: [
+      { name: 'JavaScript', level: 'advanced' },
+      { name: 'TypeScript', level: 'advanced' },
+    ],
+  };
+
+  console.log(MyResume);
+  */
+};
