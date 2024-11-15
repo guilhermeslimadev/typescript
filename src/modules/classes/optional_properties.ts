d0e533ba722272e0df767b7dee48f0bea58f2be3
Jorge Sant Ana <jorge@argus-academy.com>
2024-11-15 09:23:59 -0300

    :tada: feat: Getters e Setters

--- src/modules/classes/optional_properties.ts
+++ src/modules/classes/optional_properties.ts
@@ -1,23 +1,34 @@
 export const bootstrap = (): void => {
   class User {
     constructor(
-      private name: string,
+      private _name: string,
       private age: number,
-      private bio?: string,
+      private _xyz?: string,
     ) {}
 
-    setBio(bio: string): void {
-      this.bio = bio;
+    get name(): string {
+      return this._name;
     }
 
-    getBio(): string {
-      return this.bio || 'Biografia indisponível!';
+    set name(name: string) {
+      this._name = name;
+    }
+
+    set bio(bio: string) {
+      this._xyz = bio;
+    }
+
+    get bio(): string {
+      return this._xyz || 'Biografia indisponível!';
     }
   }
 
   const user = new User('Joana', 35, 'Programadora');
-  console.log(user.getBio());
+  console.log(user.bio);
 
-  user.setBio('Programadora e entusiasta de tecnologia!');
-  console.log(user.getBio());
+  user.bio = 'Programadora e entusiasta de tecnologia!';
+  console.log(user.bio);
+
+  user.name = 'Pedro';
+  console.log(user.name);
 };
