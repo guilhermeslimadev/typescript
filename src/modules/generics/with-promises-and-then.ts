03e54d32476c9112dc0f4e5f97ad620420e50afa
Jorge Sant Ana <jorge@argus-academy.com>
2024-11-25 09:59:32 -0300

    :tada: feat: Generics em Promises - Parte 2

--- src/modules/generics/with-promises-and-then.ts
+++ src/modules/generics/with-promises-and-then.ts
@@ -1,30 +1,43 @@
 export const bootstrap = (): void => {
-  //Promise
-  const promise = new Promise((resolve, reject) => {
-    if (false) {
-      reject('Rejeitada');
-    }
-
-    resolve('Resolvida');
-  });
-
-  // Função assíncrona (Promise)
-  async function promiseFunction() {
-    return true;
+  interface Game {
+    id: number;
+    title: string;
+    genre: string;
+    year: number;
   }
 
-  promiseFunction();
+  // Type Predicate
+  function isGameArray(data: any): data is Game[] {
+    return (
+      Array.isArray(data) &&
+      data.every(
+        (item) =>
+          typeof item.id === 'number' &&
+          typeof item.title === 'string' &&
+          typeof item.genre === 'string' &&
+          typeof item.year === 'number',
+      )
+    );
+  }
 
-  fetch('https://argus-academy.com/mock/api/games/').then((response) => {
-    if (!response.ok) {
-      console.error(
-        'Error HTTP: ',
-        `${response.status} - ${response.statusText}`,
-      );
-    }
-    response.json().then((data) => console.log(data));
-  });
+  fetch('https://argus-academy.com/mock/api/games/')
+    .then((response) => {
+      if (!response.ok) {
+        console.error(
+          'Error HTTP: ',
+          `${response.status} - ${response.statusText}`,
+        );
+      }
 
-  // asynct/await
-  // then
+      // Type Assertion
+      // return response.json() as Promise<Array<Game>>;
+      return response.json();
+    })
+    .then((data) => {
+      if (isGameArray(data)) {
+        data.forEach((game) => console.log(game.title));
+      } else {
+        console.error('Tipo de dado inesperado!');
+      }
+    });
 };
