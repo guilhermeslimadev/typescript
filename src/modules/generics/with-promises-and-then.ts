342065bc122f9208dbf299b011fd4ba609b5513e
Jorge Sant Ana <jorge@argus-academy.com>
2024-11-25 10:53:34 -0300

    :tada: feat: Generics em Promises - Parte 3

--- src/modules/generics/with-promises-and-then.ts
+++ src/modules/generics/with-promises-and-then.ts
@@ -35,7 +35,53 @@ export const bootstrap = (): void => {
     })
     .then((data) => {
       if (isGameArray(data)) {
-        data.forEach((game) => console.log(game.title));
+        //console.log(data);
+      } else {
+        console.error('Tipo de dado inesperado!');
+      }
+    });
+
+  // ---
+
+  interface Car {
+    id: number;
+    brand: string;
+    model: string;
+    type: string;
+    year: number;
+  }
+
+  // Type Predicate
+  function isCarArray(data: any): data is Car[] {
+    return (
+      Array.isArray(data) &&
+      data.every(
+        (item) =>
+          typeof item.id === 'number' &&
+          typeof item.brand === 'string' &&
+          typeof item.model === 'string' &&
+          typeof item.type === 'string' &&
+          typeof item.year === 'number',
+      )
+    );
+  }
+
+  fetch('https://argus-academy.com/mock/api/cars/')
+    .then((response) => {
+      if (!response.ok) {
+        console.error(
+          'Error HTTP: ',
+          `${response.status} - ${response.statusText}`,
+        );
+      }
+
+      // Type Assertion
+      // return response.json() as Promise<Array<Game>>;
+      return response.json();
+    })
+    .then((data) => {
+      if (isCarArray(data)) {
+        console.log(data);
       } else {
         console.error('Tipo de dado inesperado!');
       }
