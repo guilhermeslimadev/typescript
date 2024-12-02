a2839d766e8b53998de9858bf0b5d2a51d3221de
Jorge Sant Ana <jorge@argus-academy.com>
2024-12-02 11:28:14 -0300

    :tada: feat: Restrições Genéricas - Parte 2

--- src/modules/generics/generic-constraints.ts
+++ src/modules/generics/generic-constraints.ts
@@ -5,7 +5,32 @@ export const bootstrap = (): void => {
     window.speechSynthesis.speak(utterance);
   }
 
+  type ElementForReading<T> = { text: string; element: T };
+
+  function extractHTMLElementText<T extends HTMLElement>(
+    element: T,
+  ): ElementForReading<T> | null {
+    const text = element.innerText;
+
+    if (!text) {
+      console.error('Elemento não possui texto para ser lido!');
+      return null;
+    }
+    return { text, element };
+  }
+
+  const element1 = document.getElementById('course'); // HTMLElement | null
+  const element2 = document.querySelector('a'); // HTMLElement <- HTMLAnchorElement | null
+
   document.getElementById('play')?.addEventListener('click', () => {
-    speakText('Uma string qualquer para ser lida');
+    if (element1) {
+      const el = extractHTMLElementText(element1);
+      if (el) {
+        speakText(el.text);
+        el.element.style.backgroundColor = 'green';
+      }
+    }
+
+    //speakText('Uma string qualquer para ser lida');
   });
 };
